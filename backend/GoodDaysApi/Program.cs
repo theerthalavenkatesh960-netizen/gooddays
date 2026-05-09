4fe2c62deabb9e7e07d032a86a1f4e2a943323f8
Theerthala Venkatesh <theerthala.venkatesh@nationsbenefits.com>
2026-05-09 11:42:55 +0530

    fixed the daily routine failure

--- backend/GoodDaysApi/Program.cs
+++ backend/GoodDaysApi/Program.cs
@@ -8,7 +8,9 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(o =>
+        o.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
